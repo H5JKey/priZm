8bb04f064257989ae3eea447c279c13a56ec17c7
Nikolay Shirokov <shirkol2006@mail.ru>
2026-08-20 13:56:33 +0300

    refactor(project): add methods to pydantic schemas for serializing database models to json.

--- app/schemas/event.py
+++ app/schemas/event.py
@@ -1,3 +1,6 @@
+from typing import Self
+
+from infrastructure.database.models import File, Project, Render
 from pydantic import BaseModel
 
 from schemas.constraints.event import TopicConstraint
@@ -11,15 +14,6 @@ class EventBase(BaseModel):
     """
 
 
-class EventCreate(BaseModel):
-    """
-    Схема для создания записи о событии в outbox.
-    """
-
-    topic: TopicConstraint
-    message: dict  # type: ignore[type-arg]
-
-
 class GenerateRenderEvent(BaseModel):
     """
     Схема для события генерация проекта.
@@ -28,6 +22,48 @@ class GenerateRenderEvent(BaseModel):
     project_id: int
     render: RenderCreate
     file: FileLocationCreate
+
+    @classmethod
+    def get_from_database(
+        cls,
+        project: Project,
+        render: Render,
+        file: File,
+    ) -> Self:
+        file_location_create = FileLocationCreate.model_validate(file)
+        create_render_data = RenderCreate.model_validate(render)
+        return cls(
+            project_id=project.id,
+            render=create_render_data,
+            file=file_location_create,
+        )
+
+
+class EventCreate(BaseModel):
+    """
+    Схема для создания записи о событии в outbox.
+    """
+
+    topic: TopicConstraint
+    message: dict  # type: ignore[type-arg]
+
+    @classmethod
+    def get_from_database(
+        cls,
+        project: Project,
+        render: Render,
+        file: File,
+        topic: str,
+    ) -> Self:
+        message = GenerateRenderEvent.get_from_database(
+            project,
+            render,
+            file,
+        )
+        return cls(
+            topic=topic,
+            message=message.model_dump(),
+        )
 
 
 class AddRenderProjectEvent(BaseModel):
