8d29848fdaa7fae4b44da3d1380254db435f2d6b
Nikolay Shirokov <shirkol2006@mail.ru>
2026-08-21 11:14:27 +0300

    fix(project): add user_id field to project response json. Split projects views to different files.

---
+++ app/api/api_v1/projects/__init__.py
@@ -0,0 +1,13 @@
+__all__ = ("router",)
+from fastapi import APIRouter
+
+from .details_views import router as details_router
+from .list_view import router as list_router
+
+router = APIRouter(
+    prefix="/projects",
+    tags=["Project"],
+)
+
+router.include_router(list_router)
+router.include_router(details_router)
